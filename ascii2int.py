e1f78e5835b0f81e72f0d4cb1db3d61935d62429
ThoshHub <shamthosh@gmail.com>
2021-06-08 15:32:46 -0400

    initialized ascii2int file and added a few dummy test cases

---
+++ ascii2int.py
@@ -0,0 +1,10 @@
+# converts text to integers
+def ascii2int(ascii_num):
+    return 0
+
+
+# create some temporary test cases
+a = "one hundred"
+b = "four hundred"
+c = "five hundred and twenty one"
+print(ascii2int(a))
