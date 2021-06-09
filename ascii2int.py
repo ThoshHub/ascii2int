27544ea24bf7666f6a575f71181780d728103ebf
ThoshHub <shamthosh@gmail.com>
2021-06-08 22:11:17 -0400

    Updated ascii2int function to handle millions place by calculating it separately and summing it with thousands place calculation and below

--- ascii2int.py
+++ ascii2int.py
@@ -39,8 +39,6 @@ def convert_word_to_int(ascii_arr_clean):
     # for example ['four', 'hundred', 'ten'] becomes [4, 100, 10]
     int_arr = [numbers_list[number_word] for number_word in ascii_arr_clean]
     
-    print(int_arr) # debug statement
-    
     if len(int_arr) == 4:
         # this case is for four digits, for example ['four', 'hundred', 'forty', 'two']
         # the first two digits are multiplied, and the rest are added => 4(100) + 40 + 2 = 442
@@ -66,31 +64,47 @@ def ascii2int(ascii_str):
     ascii_arr = ascii_str.strip().split()  # strip extra spaces and split sentence into words
     ascii_arr_clean = [i for i in ascii_arr if i in numbers_list] # remove words any words not in the numbers_list, for example "and"
     
-    # detect if the word "thousand" is in the input string
+    # detect if the word "thousand", "million", or "billion" is in the input string
+    millions_index = ascii_arr_clean.index('million') if 'million' in ascii_arr_clean else -1
     thousands_index = ascii_arr_clean.index('thousand') if 'thousand' in ascii_arr_clean else -1
+    
     sum = 0 # value of the number that will be returned
 
     if len(ascii_arr_clean) == 1: # if the number is just one word, return the corresponding number
         return numbers_list[ascii_arr_clean[0]]
     else: # if the number has more than one word, feed it into the convert_word_to_int function
         
+        # if the number has a millions, then get the millions multiplier
+        # for example ["four", "million"] will return 4 which is multiplied by 1,000,000
+        # thousands places and less are processed below
+        if millions_index > -1:
+            million_multiplier = convert_word_to_int(ascii_arr_clean[0:millions_index])
+            sum += million_multiplier * 1000000
+            
         # if the number does have a thousands, then get the thousands multiplier
         # for example ["four", "hundred", "thousand"] will return 400 which will be multiplied by 1000
-        # hundreds and less are processed belwo
+        # hundreds and less are processed below
         if thousands_index > -1:
-            thousand_multiplier = convert_word_to_int(ascii_arr_clean[0:thousands_index])
-            print(thousand_multiplier)
+            if millions_index > -1:
+                thousand_multiplier = convert_word_to_int(ascii_arr_clean[millions_index+1:thousands_index])
+            else:
+                thousand_multiplier = convert_word_to_int(ascii_arr_clean[0:thousands_index])
             sum += thousand_multiplier * 1000
         
         # if the string has thousands, calculate hundreds alone by omitting the thousands place
         if thousands_index > -1 and thousands_index != len(ascii_arr_clean)-1:
             hundreds = convert_word_to_int(ascii_arr_clean[thousands_index+1:])
+        # if the string has millions, calculate hundreds alone by omitting the millions place
+        elif millions_index > -1 and millions_index != len(ascii_arr_clean)-1:
+            hundreds = convert_word_to_int(ascii_arr_clean[millions_index+1:])
         # process hundreds alone
-        elif thousands_index == -1:
+        elif thousands_index == -1 and millions_index == -1:
             hundreds = convert_word_to_int(ascii_arr_clean)
+        # no hundreds
         else:
             hundreds = 0
-        sum += hundreds
+        sum += hundreds  
+
     return sum
 
 # Driver code, with test cases
@@ -106,4 +120,6 @@ i = "four thousand four hundred and twelve"
 j = "four thousand four hundred and twenty one"
 k = "forty thousand four hundred and twenty one"
 l = "four hundred and twenty thousand four hundred and twenty one"
-print(ascii2int(l))
+m = "one million and forty"
+n = "six hundred million and two hundred thousand and forty two"
+print(ascii2int(n))
